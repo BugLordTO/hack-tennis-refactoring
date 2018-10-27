a2dfb2c7e471b0978cd028130e9688fac2c6b970
Somkiet <somsor@perfenterprise.com>
2018-10-27 15:26:11 +0700

    player name

--- netcore/Hack.Lib/TennisGame2.cs
+++ netcore/Hack.Lib/TennisGame2.cs
@@ -6,11 +6,9 @@ namespace Hack.Lib
     {
         private int player1Point = 0;
         private int player2Point = 0;
-
-        private string player1Result = string.Empty;
-        private string player2Result = string.Empty;
         private string player1Name;
         private string player2Name;
+        private string[] scoreTexts = { "Love", "Fifteen","Thirty", "Forty" };
 
         public TennisGame2(string player1Name, string player2Name)
         {
@@ -18,64 +16,29 @@ namespace Hack.Lib
             this.player2Name = player2Name;
         }
 
-        private string GetScoreText(int point)
-        {
-            if (point == 0)
-                return "Love";
-            else if (point == 1)
-                return "Fifteen";
-            else if (point == 2)
-                return "Thirty";
-            else if (point == 3)
-                return "Forty";
-            else throw new NotSupportedException();
-        }
-
         public string GetScore()
         {            
             if (player1Point == player2Point)
-            {
-                if(player1Point < 3)
-                    return $"{GetScoreText(player1Point)}-All";
-                else
-                    return "Deuce";
-            }
+                return player1Point < 3 ? $"{scoreTexts[player1Point]}-All" : "Deuce";
             else if (player1Point < 4 && player2Point < 4)
-            {
-                return $"{GetScoreText(player1Point)}-{GetScoreText(player2Point)}";
-            }
+                return $"{scoreTexts[player1Point]}-{scoreTexts[player2Point]}";
             else if(Math.Abs(player1Point - player2Point) == 1)
-            {
-                if (player1Point > player2Point)
-                    return "Advantage player1";
-                else
-                    return "Advantage player2";
-            }
+                return $"Advantage {GetAdvantagePlayer()}";
             else
-            {
-                if (player1Point > player2Point)
-                    return "Win for player1";
-                else
-                    return "Win for player2";
-            }
+                return $"Win for {GetAdvantagePlayer()}";
+        }
+
+        public string GetAdvantagePlayer()
+        {
+            return player1Point > player2Point ? player1Name : player2Name;
         }
 
         public void WonPoint(string player)
         {
-            if (player == "player1")
-                P1Score();
+            if (player == player1Name)
+                player1Point++;
             else
-                P2Score();
-        }
-
-        private void P1Score()
-        {
-            player1Point++;
-        }
-
-        private void P2Score()
-        {
-            player2Point++;
+                player2Point++;
         }
     }
 }
